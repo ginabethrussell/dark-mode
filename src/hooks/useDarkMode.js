0c0f06a4d2a21975b77e29f4214ac99bf9664dc9
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-12-07 15:27:36 -0500

    added usePrefersDarkMode functionality which calls useMedia hook to check user OS settings

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
+import useMedia from './useMedia';
+
+function usePrefersDarkMode() {
+    return useMedia(['(prefers-color-scheme: dark)'], [true], false);
+}
 
 const useDarkMode = () => {
-    const [darkMode,setDarkMode] = useLocalStorage('isDark', false);  
+    const [darkMode,setDarkMode] = useLocalStorage('isDark', false); 
+
+    const prefersDarkMode = usePrefersDarkMode();
+   
+    useEffect(() => {
+        if (prefersDarkMode){
+            setDarkMode(true);
+        }
+    }, [prefersDarkMode])
+    
     return [darkMode, setDarkMode];
 }
 
